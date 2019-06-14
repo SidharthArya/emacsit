c3b825b74465d49a30778d73e4ce9b68f243fe2a
Arya <arya@slackware.Slackware>
2019-06-14 12:15:18 +0530

    Feature: Added support for cask.

--- emacsit.el
+++ emacsit.el
@@ -1,5 +1,16 @@
 ;; emacsit.el -- Packages manager
 (require 'subr-x)
+(defun emacsit::initialize()
+  "Initialize emacsit"
+  (interactive)
+  (if (not (equal emacsit::cask-path nil))
+      (setenv "PATH"
+	      (concat
+	       emacsit::cask-path "/bin:"
+	       (getenv "PATH"))
+  )
+      
+  ))
 (defun emacsit::preprocess(a)
   "Preprocess list"
   (if (not (equal (member "" (split-string (format "%s" a) "/")) nil))
@@ -9,14 +20,14 @@
 
 
 (defun emacsit::getprocess(a)
-  "Preprocess Something"
+  "Preprocess Package String"
   (setq a (emacsit::preprocess a))
   (if (file-directory-p (car (cdr (split-string a "@"))))
       " "
      a))
 
 (defun emacsit::clone(a)
-  "Clone Something for me"
+  "Clone Package"
   (if (not (equal a " "))
       (progn
 	(setq emacsit::dir (car (cdr (split-string a "@"))))
@@ -26,7 +37,7 @@
   a
   )
 (defun emacsit::clone-byte-compile(a)
-  "Clone Something for me"
+  "Clone and Byte Compile Package"
   (if (not (equal a " "))
       (progn
 	(setq emacsit::dir (car (cdr (split-string a "@"))))
@@ -44,27 +55,27 @@
 	(async-shell-command (format "cd %s && git pull " emacsit::dir)))
     )
   )
+
+
 (defun emacsit::update-all()
   "Update all packages"
-  (interactive)				
+  (interactive)
   (mapcar 'emacsit::update (mapcar 'emacsit::preprocess emacsit::packages))
   )
+
+
 (defun emacsit::byte-compile-all()
   "Byte Compile"
   (interactive)
     (mapcar 'emacsit::clone-byte-compile (mapcar 'emacsit::preprocess emacsit::packages)))
-  
+
+
 (defun emacsit::loadPackage(a)
-  "Something for me"
+  "Load Package"
   (setq a (car (cdr (split-string a "@"))))
   (add-to-list 'load-path a)
   (mapcar (lambda (b) (if (not (equal b "")) (add-to-list 'load-path b))) (split-string (string-trim (shell-command-to-string (format "find %s -regextype sed -regex '.*/lisp'" a)) "\n")))
     (mapcar (lambda (b) (if (not (equal b "")) (add-to-list 'load-path b))) (split-string (string-trim (shell-command-to-string (format "find %s -regextype sed -regex '.*/lib'" a)) "\n")))
-;;  (if (file-directory-p (concat a "/lisp"))
- ;;     (add-to-list 'load-path (format "%s/lisp" a))
-  ;;  (add-to-list 'load-path (format "%s" a)))
-  ;;(if (file-directory-p (concat a "/lib"))
-   ;;   (add-to-list 'load-path (format "%s/lib" a)))
   )
 
 
@@ -74,6 +85,8 @@
   (mapcar 'emacsit::clone (mapcar 'emacsit::getprocess emacsit::packages))
   (emacsit::load)
   )
+
+
 (defun emacsit::load()
   "Load all packages listed"
   (interactive)
@@ -85,8 +98,12 @@
   "Clean packages"
   
   )
+
+
 (defun emacsit::dirclean()
   "Directory Clean"
 
-)
+  )
+
+(emacsit::initialize)
 (provide 'emacsit)
